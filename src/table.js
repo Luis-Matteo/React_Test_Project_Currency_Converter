e88c4ce187e85ecb28fc361b9dfccf2f9dea0dcd
RayDev1988 <devsops2022@gmail.com>
2022-09-13 23:40:46 -0700

    react-test-project

---
+++ src/table.js
@@ -0,0 +1,20 @@
+// /* eslint-disable array-callback-return */
+// import React from "react";
+// import TableList from "./tablelist";
+
+// function Table(allData) {
+//     return (
+//         <table>
+//             <tr>
+//                 <th>Phone_Work</th>
+//                 <th>First_Name</th>
+//                 <th>Last_Name</th>
+//             </tr>
+//             {allData.map((data, index) => {
+//                 <TableList key = {index} data = {data} />
+//             })}
+//         </table>
+//     )
+// }
+
+// export default Table
---
+++ src/tablelist.js
@@ -0,0 +1,13 @@
+// import React from "react";
+
+// function TableList(data) {
+//     return (
+//         <tr>
+//             <td>{data.phone_work}</td>
+//             <td>{data.first_name}</td>
+//             <td>{data.last_name}</td>
+//         </tr>
+//     );
+// }
+
+// export default TableList;
